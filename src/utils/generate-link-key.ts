ea8ceb595aaffc21505e3014d6fdc0711d4af9ca
Lida Feoktistova <lida.feoktistova@mail.ru>
2024-11-26 16:51:58 +0300

    use redis

--- src/utils/generate-link-key.ts
+++ src/utils/generate-link-key.ts
@@ -1,6 +1,6 @@
 import { generate } from 'randomstring';
 
-export default function generateLinkKey(length: number) {
+export default function generateLinkKey(length: number): string {
   return generate({
     length,
     charset: 'alphanumeric',
